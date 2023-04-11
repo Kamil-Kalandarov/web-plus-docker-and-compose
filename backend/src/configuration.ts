e3b8a8f5226b64c0908d138354d546cf7c620626
Kamil-Kalandarov <iam@kalandarov-13.ru>
2023-04-11 15:52:22 +0300

    first start on server

--- backend/src/configuration.ts
+++ backend/src/configuration.ts
@@ -5,17 +5,17 @@ import { WishList } from "./wishlists/entities/wishList.entity";
 
 /* ФАЙЛ КОНФИГУРАЦИИ ДЛЯ ПОДКЛЮЧЕНИЯ ПРИЛОЖЕНИЯ К БД (данные берутся из файла .env) */
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT, 10) || 4000,
   database: {
     type: "postgres",
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT || 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     entities: [User, Offer, Wish, WishList],
     synchronize: true,
   },
-  jwtSecretKey: process.env.JWT_SECRET_KEY,
+  jwtSecretKey: process.env.JWT_SECRET,
   jwtLiveTime: process.env.JWT_LIVE_TIME,
 });
